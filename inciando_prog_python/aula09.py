e0b47380d4f9b6e6e997024c5fc6b710d7573f10
André Henrique Sousa <61280435+ahsdesenvolvimento@users.noreply.github.com>
2023-09-18 17:09:25 -0300

    bloco de códigos com if/elif/else
    Utilizando condições if/elif/else em Python

---
+++ inciando_prog_python/aula09.py
@@ -0,0 +1,47 @@
+# Operações condicionais
+
+
+# if / elif      / else
+# se / se não se / se não
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+
+    print(12341234)
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não digitou nem entrar e nem sair.')
+
+print('FORA DOS BLOCOS\n\n')#não pertence ao bloco de decisões(if/elif/else)
+print('##########################################################\n')
+
+# continuando com operadores condicionais if/elif/else
+# apenas uma condição da estrutura de decisão será exibida
+# mesmo que todas as condições sejam verdadeiras
+# a primeira condição que for verdadeira será executada e as demais 
+# não serão verificadas
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
