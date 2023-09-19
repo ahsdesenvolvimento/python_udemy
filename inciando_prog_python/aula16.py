50a71e3feb4cd8e3ffcd61305d9bdefaf39197c9
André Henrique Sousa <61280435+ahsdesenvolvimento@users.noreply.github.com>
2023-09-19 11:16:30 -0300

    Interpolação de strings com % em Python
    Realizando a interpolação de strings com % em Python

---
+++ inciando_prog_python/aula16.py
@@ -0,0 +1,19 @@
+#Interpolação básica de strings
+#s - string
+#d e i - int
+#f - float
+#x e X - Hexadecimal (ABCDEF0123456789)
+#utilizar a % e o tipo do valor (i para int, s para string, etc...)
+#no final colocar % e entre parenteses os nomes das variaveis
+
+#nome = 'André'
+#preco = 2500.9872938
+#variavel = '%s, o preço é R$%.2f' % (nome, preco)
+#print(variavel)
+#print('O hexadecimal de %d é %08X' % (3700, 3700))
+
+time = 'Rio Branco'
+ano_fundacao = 1913
+dados_time = '%s, foi fundado em %i' % (time, ano_fundacao)
+print(dados_time)
+
