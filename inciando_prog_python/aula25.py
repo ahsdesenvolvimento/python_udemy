71a61efeaed306fb01b05dbca1210ec8a71e0c38
André Henrique Sousa <61280435+ahsdesenvolvimento@users.noreply.github.com>
2023-09-21 10:50:53 -0300

    Estrutura de repetição While
    Iniciando com estruturas de repetição utilizando o while em Python

---
+++ inciando_prog_python/aula25.py
@@ -0,0 +1,47 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+"""
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break             # uma maneira de parar o loop
+
+print('Acabou')
+
+"""
+"""
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
+
+"""
+# Treinando While :
+cont= 0
+nome1 = ''
+nome2 = ''
+nome3 = ''
+
+while cont <3:
+    cont = cont + 1
+    nome = input('digite seu nome: ')
+    if cont == 1:
+        nome1 = nome
+    elif cont == 2:
+        nome2 = nome
+    elif cont == 3:
+        nome3 = nome
+print(f'o primeiro nome é {nome1}')
+print(f'o segundo nome é {nome2}')
+print(f'o terceiro nome é {nome3}')
