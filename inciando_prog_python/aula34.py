d570ea3b9d70683cdda4ee840373b1279d9fb8b5
André Henrique Sousa <61280435+ahsdesenvolvimento@users.noreply.github.com>
2023-09-25 11:33:58 -0300

    Exercício com while dentro de while
    exercicio laços de repetição aninhados (while dentro de while)

---
+++ inciando_prog_python/aula34.py
@@ -0,0 +1,11 @@
+#exercicio laços de repetição aninhados (while dentro de while)
+linhas = 2
+colunas = 2
+ 
+linha = 1
+while linha <= linhas:
+    coluna = 1
+    while coluna <= colunas:
+        print(linha, coluna)
+        coluna += 1
+    linha += 1
